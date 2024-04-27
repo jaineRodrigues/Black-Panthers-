aad54cb117279514d31ed48050355f9ce5c42894
jaineRodrigues <jainer469@gmail.com>
2024-04-27 18:29:12 -0300

    fix: logic for cpf or

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ document.querySelector('#menu-btn').onclick = () => {
   navbar.classList.toggle('active');
 }
 
+function toggleDocumentType() {
+  var selectedValue = document.getElementById('lead_type').value;
+  var cpfField = document.getElementById('cpf');
+  var cnpjField = document.getElementById('cnpj');
+
+  if (selectedValue === 'Pessoa Física') {
+    cpfField.style.display = 'block';
+    cnpjField.style.display = 'none';
+    cpfField.required = true;
+    cnpjField.required = false;
+  } else if (selectedValue === 'Pessoa Jurídica') {
+    cpfField.style.display = 'none';
+    cnpjField.style.display = 'block';
+    cpfField.required = false;
+    cnpjField.required = true;
+  } else {
+    cpfField.style.display = 'none';
+    cnpjField.style.display = 'none';
+    cpfField.required = false;
+    cnpjField.required = false;
+  }
+}
